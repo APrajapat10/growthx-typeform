9f8c22f8e826c028e5bad63cb83fc80ad6c085b8
Abhishek Prajapat <coolprajapat@gmail.com>
2022-11-27 16:30:27 +0530

    WidthAdjustment WIP

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
+import { useState } from "react";
+
 function App() {
+  const [submitScreen, setSubmitScreen] = useState(false);
+  const [widthArray, setWidthArray] = useState([0, 0, 0, 0, 0, 0, 0]);
+  const topWidthBar = widthArray.reduce((partialSum, a) => partialSum + a, 0);
   return (
     <div className="bg-black pt-5 p-4 relative flex flex-col h-screen overflow-y-auto snap-y snap-mandatory">
+      {!submitScreen && (
+        <>
+          <div className="fixed top-0 left-0 w-full h-1 bg-[#0077ff] opacity-50"></div>
+          <div
+            className="fixed top-0 left-0 h-1 bg-[#0077ff] transition-all ease-in-out duration-500"
+            style={{
+              width: `${(topWidthBar / 7) * 100}%`,
+            }}
+          ></div>
+        </>
+      )}
       <div className="w-24 h-auto fixed">
         <img src="/GX-logo.png" alt="growthx-logo" className="object-contain" />
       </div>
